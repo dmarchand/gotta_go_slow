d651d73a2b499e6ecb227f7ee841df7af070de4b
dmarchand <dan.marchand@gmail.com>
2020-08-16 12:40:31 -0400

    Basic debris movement

---
+++ Assets/Scripts/Actors/Enemies/DebrisFallingController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebrisFallingController : MonoBehaviour
+{
+    private DebrisConfigurationController debrisConfigurationController;
+
+    private void Awake() {
+        debrisConfigurationController = GetComponent<DebrisConfigurationController>();    
+
+        if(debrisConfigurationController == null) {
+            throw new System.MissingMemberException("No DebrisConfigurationController found");
+        }
+    }
+
+    private void Update() {
+        transform.Translate(new Vector3(0, CalculateFallSpeed(), 0));
+    }
+
+    private float CalculateFallSpeed() {
+        float fallSpeed = debrisConfigurationController.BaseFallSpeed;
+        fallSpeed *= Time.deltaTime;
+        // Add base game speed here
+
+        return -fallSpeed;
+    }
+}
